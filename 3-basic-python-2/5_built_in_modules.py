1d5d45102c6cf27d9cda8c192973f53a36d3e10f
Mostafizur06 <mostafizur1102@gmail.com>
2023-09-28 20:19:44 +0600

    Module 3 added

---
+++ 3-basic-python-2/5_built_in_modules.py
@@ -0,0 +1,13 @@
+from math import *
+from random import *
+from time import sleep,time
+# result = ceil(4.00001)
+result = floor(4.9999)
+print(result)
+print(random())
+print(randint(1,100))
+# sleep(4)
+print(choice(['sakib', 'mash', 'mush', 'rid', 'musta'])) 
+print(time())
+
+# https://docs.python.org/3/py-modindex.html
