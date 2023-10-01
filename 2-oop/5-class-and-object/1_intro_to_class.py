a1435739639f124ca9510040896ee1ff4a2b5208
Mostafizur06 <mostafizur1102@gmail.com>
2023-10-01 11:05:05 +0600

    object and class module started

---
+++ 2-oop/5-class-and-object/1_intro_to_class.py
@@ -0,0 +1,14 @@
+class Phone:
+    nam = 'Google Pixel 5'
+    camera = '12+12'
+    battery = '4046 MAH'
+    price = 72000
+    warranty = '1 year'
+
+
+my_class = Phone()
+
+print(my_class.nam)
+print(my_class.camera)
+print(my_class.battery)
+print(my_class.warranty)
